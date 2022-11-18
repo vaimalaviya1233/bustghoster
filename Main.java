e083665e7bb39d5f4ed711fdb5f4331fa4fc33db
Vaibhav Malaviya <55515799+vaimalaviya1233@users.noreply.github.com>
2022-11-18 19:24:27 +0000

    change

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.nio.file.*;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println();
+    Scanner scn = new Scanner("ready.txt");
+    String cmd[] = new String[3764];
+    int lin = 0;
+    while(scn.hasNextLine()){
+      lin++;
+    }
+    System.out.println(lin);
+    //while(scn.hasNextLine()){}
   }
 }
