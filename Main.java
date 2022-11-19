4108f159fbf1f2666d762ff6bb765fbd99486efa
Vaibhav Malaviya <55515799+vaimalaviya1233@users.noreply.github.com>
2022-11-19 01:32:44 +0000

    fix

--- Main.java
+++ Main.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
-import java.nio.file.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import static java.lang.System.*;
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     System.out.println("Hello world!");
     System.out.println();
-    Scanner scn = new Scanner("ready.txt");
-    String cmd[] = new String[3764];
+    Scanner scn = new Scanner(new File("ready.txt"));
+    //String cmd[] = new String[3764];
+    String cmd = "";
     int lin = 0;
-    while(scn.hasNextLine()){
+    while (scn.hasNextLine()) {
+      cmd = cmd + (scn.nextLine() + "; ");
+      //out.println(cmd[lin]);
       lin++;
+      
     }
-    System.out.println(lin);
-    //while(scn.hasNextLine()){}
+    out.println(cmd.toString());
+    // while(scn.hasNextLine()){}
+    PrintWriter writer = new PrintWriter("output.txt");
+    writer.println(cmd);
+    writer.close();
   }
 }
